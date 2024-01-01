21484e8ae7448da1a43dc6696b32494431d941d8
Johnny <143060590+Johnnyallen07@users.noreply.github.com>
2024-01-02 05:22:52 +0800

    Add files via upload

--- src/maps/PieceResourceMap.java
+++ src/maps/PieceResourceMap.java
@@ -16,32 +16,32 @@ public class PieceResourceMap {
         String absoluteProjectPath = new File("").getAbsolutePath();
 
         pieceNameStringMap.put(new Pair<>(PieceName.bishop, PieceColor.WHITE),
-                absoluteProjectPath+"\\src\\pieces\\bishop_white.png");
+                absoluteProjectPath+"/src/pieces/bishop_white.png");
         pieceNameStringMap.put(new Pair<>(PieceName.pawn, PieceColor.WHITE),
-                absoluteProjectPath+"\\src\\pieces\\pawn_white.png");
+                absoluteProjectPath+"/src/pieces/pawn_white.png");
         pieceNameStringMap.put(new Pair<>(PieceName.king, PieceColor.WHITE),
-                absoluteProjectPath+"\\src\\pieces\\king_white.png");
+                absoluteProjectPath+"/src/pieces/king_white.png");
         pieceNameStringMap.put(new Pair<>(PieceName.knight, PieceColor.WHITE),
-                absoluteProjectPath+"\\src\\pieces\\knight_white.png");
+                absoluteProjectPath+"/src/pieces/knight_white.png");
         pieceNameStringMap.put(new Pair<>(PieceName.queen, PieceColor.WHITE),
-                absoluteProjectPath+"\\src\\pieces\\queen_white.png");
+                absoluteProjectPath+"/src/pieces/queen_white.png");
         pieceNameStringMap.put(new Pair<>(PieceName.rook, PieceColor.WHITE),
-                absoluteProjectPath+"\\src\\pieces\\rook_white.png");
+                absoluteProjectPath+"/src/pieces/rook_white.png");
 
 
 
         pieceNameStringMap.put(new Pair<>(PieceName.bishop, PieceColor.BLACK),
-                absoluteProjectPath+"\\src\\pieces\\bishop_black.png");
+                absoluteProjectPath+"/src/pieces/bishop_black.png");
         pieceNameStringMap.put(new Pair<>(PieceName.pawn, PieceColor.BLACK),
-                absoluteProjectPath+"\\src\\pieces\\pawn_black.png");
+                absoluteProjectPath+"/src/pieces/pawn_black.png");
         pieceNameStringMap.put(new Pair<>(PieceName.king, PieceColor.BLACK),
-                absoluteProjectPath+"\\src\\pieces\\king_black.png");
+                absoluteProjectPath+"/src/pieces/king_black.png");
         pieceNameStringMap.put(new Pair<>(PieceName.knight, PieceColor.BLACK),
-                absoluteProjectPath+"\\src\\pieces\\knight_black.png");
+                absoluteProjectPath+"/src/pieces/knight_black.png");
         pieceNameStringMap.put(new Pair<>(PieceName.queen, PieceColor.BLACK),
-                absoluteProjectPath+"\\src\\pieces\\queen_black.png");
+                absoluteProjectPath+"/src/pieces/queen_black.png");
         pieceNameStringMap.put(new Pair<>(PieceName.rook, PieceColor.BLACK),
-                absoluteProjectPath+"\\src\\pieces\\rook_black.png");
+                absoluteProjectPath+"/src/pieces/rook_black.png");
 
     }
 
